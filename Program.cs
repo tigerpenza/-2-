fd0a1838e70cc0a69eee7402919ce8a65e6f081e
evgenii <tigerpenza@gmail.com>
2022-08-24 13:18:47 +0300

    нахождение остатка числа , и большего числа и цифры а так же пример (метода

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+int Big (int number)
+{
+
+     int ed = number % 10;
+     int dec = number / 10;
+
+     int max;
+     if (ed > dec) max = ed;
+     
+     else max = dec;
+
+     return max; 
+
+}
+    
+    int randNumber = new Random().Next(10,100);
+    int BigN = Big (randNumber);
+
+    Console.WriteLine($"Bign of {randNumber} is {BigN}" );
